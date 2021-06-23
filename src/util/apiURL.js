b5092e28d9265a08eb5a7852e020c3c40f793af7
EsayH <ez@EsayHs-Mac-mini.local>
2021-06-23 16:16:46 -0400

    fix apiURL

--- src/util/apiURL.js
+++ src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
 	return window.location.hostname === 'localhost'
 		? 'http://localhost:9090'
-		: 'https://pure-sea-03440.herokuapp.com/'
+		: 'https://pure-sea-03440.herokuapp.com'
 }
